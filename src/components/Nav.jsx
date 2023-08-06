0f48ef22417fcee6d97cf6bc29e33b6c339d67bf
Demma <denisssendagire22@gmail.com>
2023-08-06 23:44:02 +0300

    Course Component POST

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -11,10 +11,12 @@ export default function Nav() {
       <Link to={"/staff"}>Staff</Link>
       <Link to={"/club"}>staff-form</Link>
       <Link to={"/department"}>department</Link>
+      <Link to={"/course"}>Course</Link>
       <Routes>
         <Route path='/' element={<h1>Homepage</h1>}></Route>
         <Route path='/staff' element={<Staff />}></Route>
         <Route path='/club' element={<Club />}></Route>
+        <Route path='/course' element={<Course />}></Route>
       </Routes>
     </div>
   )
